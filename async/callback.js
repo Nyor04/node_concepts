8f17883daaa7ef04aee67afd217cd1132a5ff6a3
Bryan Ureña <bry.alexander03@gmail.com>
2024-11-16 12:18:28 -0400

    se agrega comentario

--- async/callback.js
+++ async/callback.js
@@ -1,3 +1,8 @@
+//Los callbacks en JavaScript son funciones que se pasan como argumentos a otras funciones 
+// y se ejecutan después de que la función a la que se han pasado ha terminado su ejecución. 
+// Es una forma de manejar código asincrónico, permitiendo que ciertas tareas se completen 
+// antes de que otras se ejecuten.
+
 function soy_asincrona(callback){
     setTimeout(() => {
         console.log("Este console log es asincrono!!");
